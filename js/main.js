c052dbc6aa94ffba6340d90bf9239af2b5805521
cem aytac <cenko@MacBook-Air-3.local>
2020-08-04 22:54:39 -0400

    updated variables

--- js/main.js
+++ js/main.js
@@ -5,21 +5,28 @@ const colors = {
   playerX: 'red',
   playerO: 'blue'
 }
-const winningCombos = [board[0], board[1], board[2] || board[3], board[4], board[5] || board[6], board[7], board[8] || board[0], board[4], board[8] || board[2], board[4], board[6] || board[0], board[3], board[6] || board[1], board[4], board[7] || board[2], board[5], board[8]];
+
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8]
+];
 
 /*------Variables (state)------*/
 
 // Variables might include (board/turn/winner)
 
-const board = [null, null, null, null, null, null, null, null, null];
-
-const turns
-
-const winner;
+const board, turn, winner;
 
 /*------Cached Element References------*/
 
 // You might choose to put your game status here
+
 
 /*------Event Listeners------*/
 
