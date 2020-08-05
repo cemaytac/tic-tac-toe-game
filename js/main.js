0bb48ffd71c17cfe623364011a20af4b007bbee2
cem aytac <cenko@MacBook-Air-3.local>
2020-08-04 22:43:03 -0400

    starter code

--- js/main.js
+++ js/main.js
@@ -1,30 +1,55 @@
-/*----- constants -----*/
+/*------Constants------*/
 
 const colors = {
   null: 'white',
   playerX: 'red',
   playerO: 'blue'
 }
-const winningCombos = [board[0], board[1], board[2] || board[3], board[4], board[5] || board[6], board[7], board[8] || board[0], board[4], board[8] || board[2], board[4], board[6] || board[0], board[3], board[6] || board[1], board[4], board[7] || board[2], board[5], board[8]]
+const winningCombos = [board[0], board[1], board[2] || board[3], board[4], board[5] || board[6], board[7], board[8] || board[0], board[4], board[8] || board[2], board[4], board[6] || board[0], board[3], board[6] || board[1], board[4], board[7] || board[2], board[5], board[8]];
 
-/*----- app's state (variables) -----*/
+/*------Variables (state)------*/
+
+// Variables might include (board/turn/winner)
+
 const board = [null, null, null, null, null, null, null, null, null];
 
 const turns
 
 const winner;
 
-/*----- cached element references -----*/
-const topRow = document.getElementById('top');
-const middleRow = document.getElementById('middle');
-const bottomRow = document.getElementById('bottom');
+/*------Cached Element References------*/
+
+// You might choose to put your game status here
+
+/*------Event Listeners------*/
+
+// This is where you should put the event listener
+// for a mouse-click
+
+/*------Functions------*/
 
 
-/*----- event listeners -----*/
+// Some functions you might choose to use:
+
+// Initialization function:
+// Where you set your initial state, setting up 
+// what the board will look like upon loading
+
+// On-Click function:
+// Set up what happens when one of the elements
+// is clicked
 
 
+// Check winner function:
+// Checks the current state of the board for
+// a winner and changes the state of the winner
+// variable if so
 
-/*----- functions -----*/
+
+// Render function:
+// Displays the current state of the board
+// on the page, updating the elements to reflect
+// either X or O depending on whose turn it is
 
 // 1) Define required constants
 
